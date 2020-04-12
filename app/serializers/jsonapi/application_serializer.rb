e6b89af0bca2066138ea79d68d865b50e0ee7748
Tom Bosmans <tom.94.bosmans@gmail.com>
2020-04-12 19:17:45 +0200

    rubocop cleanup

--- app/serializers/jsonapi/application_serializer.rb
+++ app/serializers/jsonapi/application_serializer.rb
@@ -7,7 +7,7 @@ class Jsonapi::ApplicationSerializer
 
   class << self
     include Rails.application.routes.url_helpers
-    
+
     def default_url_options
       ActionController::API.default_url_options
     end
